7fe6587fb0af2cefa9770d4bcc3bfa00c1065d84
David Sands <dsands@portalflare.com>
2013-04-26 15:14:39 -0400

    fixed DependencyInjection namespace

--- DependencyInjection/PortalFlareMetadataExtension.php
+++ DependencyInjection/PortalFlareMetadataExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PortalFlare\Bundle\MetadataBundle\DependencyInjection;
+namespace PortalFlare\MetadataBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
